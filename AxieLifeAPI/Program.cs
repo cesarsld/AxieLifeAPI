76bdbc225fe606cd7ae408aab1383655ca579906
cesarsld <jaimes.cesar88@gmail.com>
2019-04-26 01:10:03 +0100

    port change

--- AxieLifeAPI/Program.cs
+++ AxieLifeAPI/Program.cs
@@ -21,6 +21,7 @@ namespace AxieLifeAPI
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseUrls("http://*:1337");
     }
 }
